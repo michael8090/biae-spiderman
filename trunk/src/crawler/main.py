e3325fc02d9441747d879886ac0ed6f0623fd845
michael8090@gmail.com <michael8090@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-09 14:44:39 +0000

    a little bit change in main.py, which makes the test a little bit easier.
    note that the code for Status doesn't work right now, so please comment the line for status if you want to test your own class.

--- trunk/src/crawler/main.py
+++ trunk/src/crawler/main.py
@@ -2,21 +2,15 @@
 
 from WeiboUser import WeiboUser
 from WeiboFollower import WeiboFollower
+from Status import Status
 from conf import *
 
 if __name__ == '__main__':
-    #for each WeiboUser ID, crawl its Profile:
     for lUserID in gUsersVec:
         try:
-            lWeiboUser = WeiboUser(lUserID)
-            lWeiboUser.process()
-        except Exception, e:
-            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
-    
-    #for each WeiboUser ID, crawl its followers' IDs:
-    for lUserID in gUsersVec:
-        try:
-            lWeiboFollower = WeiboFollower(lUserID)
-            lWeiboFollower.process()
+            WeiboFollower(lUserID).process()
+            WeiboUser(lUserID).process()
+            Status(lUserID).process()
+
         except Exception, e:
             print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
