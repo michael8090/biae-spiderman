539e4baaeb5940b2be5e994b596ef10b772773d1
michael8090@gmail.com <michael8090@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-09 15:12:07 +0000

    change the main back to the original structure, making it stronger.

--- trunk/src/crawler/main.py
+++ trunk/src/crawler/main.py
@@ -6,11 +6,25 @@ from Status import Status
 from conf import *
 
 if __name__ == '__main__':
+    #for each WeiboUser ID, crawl its Profile:
     for lUserID in gUsersVec:
         try:
-            WeiboFollower(lUserID).process()
-            WeiboUser(lUserID).process()
+            lWeiboUser = WeiboUser(lUserID)
+            lWeiboUser.process()
+        except Exception, e:
+            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
+    
+    #for each WeiboUser ID, crawl its followers' IDs:
+    for lUserID in gUsersVec:
+        try:
+            lWeiboFollower = WeiboFollower(lUserID)
+            lWeiboFollower.process()
+        except Exception, e:
+            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
+    
+    #for each WeiboUser ID, crawl its Status:
+    for lUserID in gUsersVec:
+        try:
             Status(lUserID).process()
-
         except Exception, e:
             print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
