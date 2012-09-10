06dc420ab85a110dbd6ee3a38012f608b241c0c2
michael8090@gmail.com <michael8090@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-10 14:00:17 +0000

    完成了status的实现

--- trunk/src/crawler/main.py
+++ trunk/src/crawler/main.py
@@ -7,20 +7,20 @@ from conf import *
 
 if __name__ == '__main__':
     #for each WeiboUser ID, crawl its Profile:
-    for lUserID in gUsersVec:
-        try:
-            lWeiboUser = WeiboUser(lUserID)
-            lWeiboUser.process()
-        except Exception, e:
-            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
-    
-    #for each WeiboUser ID, crawl its followers' IDs:
-    for lUserID in gUsersVec:
-        try:
-            lWeiboFollower = WeiboFollower(lUserID)
-            lWeiboFollower.process()
-        except Exception, e:
-            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
+##    for lUserID in gUsersVec:
+##        try:
+##            lWeiboUser = WeiboUser(lUserID)
+##            lWeiboUser.process()
+##        except Exception, e:
+##            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
+##    
+##    #for each WeiboUser ID, crawl its followers' IDs:
+##    for lUserID in gUsersVec:
+##        try:
+##            lWeiboFollower = WeiboFollower(lUserID)
+##            lWeiboFollower.process()
+##        except Exception, e:
+##            print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lUserID, str(e)))
     
     #for each WeiboUser ID, crawl its Status:
     for lUserID in gUsersVec:
