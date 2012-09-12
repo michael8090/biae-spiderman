aac4b098dd12b8dfc3425c00263131adc5ab7de5
hiddenpublic@gmail.com <hiddenpublic@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-12 12:51:58 +0000

    ^ util: add 'get_weibo_client' ^ break 'comment.py' into: task, api & dao ^ add 'dao/user.py' (stub, no implementation)

--- trunk/src/crawler/comment.py
+++ trunk/src/crawler/comment.py
@@ -5,83 +5,45 @@ import types
 from WeiboClient import WeiboClient
 from PublicToken import PublicToken
 import util
-from util import parse_weibo_time_string, parse_long
+from dao.comment import CommentDao
 
 
-class WeiboComment:
-    SQL_TEMPLATE = """
-INSERT INTO status_comment (comment_id, commented_status_id, user_id, created_time, 
-    `text`, source, mid, replied_to_comment_id, INSERT_TIMESTAMP)
-VALUES (%s, %s, %s, FROM_UNIXTIME(%s), %s, %s, %s, %s, NULL)
-ON DUPLICATE KEY UPDATE comment_id = comment_id;
-""".strip()
+class WeiboCommentAPI:
 
     API = 'comments/show'
     PAGE_SIZE = 200
     
-    def __init__(self, status_id):
-        self._status_id = status_id
-        self._wclient = WeiboClient(PublicToken.getPublicToken()[0])
+    def __init__(self, client):
+        self._client = client
     
 
     #fetch from Weibo and call sendToDB
-    def process(self):
+    def get_comments_on_status(self, status_id):
         
         api_params = {
-                     'id' : self._status_id,
+                     'id' : status_id,
                      #mPublicToken is a list:['uid', 'access_token']
-                     'access_token' : self._wclient.mPublicToken[1],
+                     'access_token' : self._client.mPublicToken[1],
                      'count' : self.PAGE_SIZE
                      }
 
-        j_result = self._wclient.fetchUsingAPI(self.API, api_params)
-        if type(j_result) == types.ListType and len(j_result) > 0:
-            self._sendToDB(j_result)
+        j_result = self._client.fetchUsingAPI(self.API, api_params)
+        return j_result
    
-    #send json data to database
-    def _sendToDB(self, j_comments):
-        assert(type(j_comments) == types.ListType)
 
-        status_id = j_comments[0]['status']['id']
-
-        rows = []
-        for jComment in j_comments:
-            if not jComment.has_key('user'):
-                print '>< user N/A for status %d repost %d' % (status_id, jComment['id'])
-                continue
-
-            if not jComment.has_key('status'):
-                print '>< orgin N/A for status %d repost %d' % (status_id, jComment['id'])
-                continue
-            
-            rows.append(self._mapRow(jComment))
-
-        try:
-            conn = util.get_crawler_connection()
-            cursor = conn.cursor()
-            cursor.executemany(self.SQL_TEMPLATE, rows)
-            cursor.close()
-            conn.commit()
-            conn.close()
-        except Exception, e:
-            raise
-            #print 'Error when insert WeiboFollower into Database for uid = %s because of: %s' % (self.mUid, e) 
-    
-
-    @staticmethod
-    def _mapRow(o):
-        
-        reply_comment = o.get('reply_comment')
-        reply_comment_id = reply_comment['id'] if not reply_comment is None else 0
-        
-        return (o['id'], o['status']['id'], o['user']['id'],
-                 parse_weibo_time_string(o['created_at']), 
-                 o['text'], o['source'], 
-                 parse_long(o['mid']), reply_comment_id
-                 )        
 
 
 if __name__ == '__main__':
-    crawler = WeiboComment(11142488790L)
-    crawler.process()
+    
+    try:
+        conn = util.get_crawler_connection()
+        commentDao = CommentDao(conn)
+        
+        client = util.get_weibo_client()
+        crawler = WeiboCommentAPI(client)
+        j_comments = crawler.get_comments_on_status(11142488790L)
+        if type(j_comments) == types.ListType and len(j_comments) > 0:
+            commentDao.insert_comments(j_comments)
 
+    finally:
+        conn.close()
--- trunk/src/task/repost.py
+++ trunk/src/task/repost.py
@@ -10,6 +10,7 @@ from dao.status import StatusDao
 from crawler import WeiboClient
 from crawler.PublicToken import PublicToken
 import util
+from dao.user import UserDao
 
 
 class RepostCrawlTask:
@@ -22,20 +23,27 @@ class RepostCrawlTask:
             conn = util.get_crawler_connection()
             statusDao = StatusDao(conn)
             repostDao = RepostDao(conn)
+            userDao = UserDao(conn)
     
-            PublicToken.getPublicToken()
-            client = WeiboClient(PublicToken.getPublicToken()[0])
+            client = util.get_weibo_client()
             crawler = WeiboRepostAPI(client)
 
             statuses = statusDao.getAllStatuses()
 
-            j_reposts = []
+            j_all_reposts = []
+            j_all_authors = []
+
             for status in statuses:
-                j_reposts.extend(crawler.fetch(status['id']))
+                j_reposts = crawler.get_reposts_of_status(status['id'])
+                
+                j_authors = (repost['user'] for repost in j_reposts) 
+                
+                j_all_reposts.extend(j_reposts)
+                j_all_authors.extend(j_authors)
     
-            j_reposts = crawler.fetch(11142488790L)
-            repostDao.insert_reposts(j_reposts)
+            repostDao.insert_reposts(j_all_reposts)
+            userDao.insert_users(j_all_authors)
 
         finally:    
             conn.close()
-        
+
