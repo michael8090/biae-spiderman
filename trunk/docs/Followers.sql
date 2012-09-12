1ae6e32e9e9fdb76b54dde679cdb5236dba599c6
michael8090@gmail.com <michael8090@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-12 16:02:18 +0000

    bug fix

--- trunk/docs/Followers.sql
+++ trunk/docs/Followers.sql
@@ -3,5 +3,6 @@ CREATE TABLE Followers(
 	id_user BIGINT NOT NULL,
 	id_follower BIGINT NOT NULL,
 	is_ActiveFun tinyint NOT NULL DEFAULT 0, 
+	INSERT_TIMESTAMP TIMESTAMP DEFAULT 0,
 	PRIMARY KEY (id_user,id_follower)
 );
