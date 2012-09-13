d8ab25dc5086f47040482814d6b2a55dfcba75a9
shezepo0409@gmail.com <shezepo0409@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-13 13:50:28 +0000

    Improved!

--- trunk/src/crawler/Followers.py
+++ trunk/src/crawler/Followers.py
@@ -1,12 +1,15 @@
 '''Followers's methods
 '''
-import MySQLdb
+
 import json
+import MySQLdb
 import types
-from WeiboClient import WeiboClient
+
+import util
 from crawler.conf import *
+from dao.user import UserDao
 from PublicToken import PublicToken
-from FollowersFollowing import FollowersFollowingV
+from WeiboClient import WeiboClient
 
 def getNULL(s):
     if s=='':
@@ -22,15 +25,6 @@ class Followers(WeiboClient):
                 
     mAPI = 'friendships/followers'
     
-# for followers's profile
-    mSQLStatement_fp = "INSERT INTO WeiboUser (idUser, screen_name, name, province, city, \
-            location, description, url, profile_image, domain, gender, avatar_large, \
-            verified, verified_reason, INSERT_TIMESTAMP, LAST_UPDATE_TIMESTAMP) VALUES %s ON DUPLICATE KEY UPDATE LAST_UPDATE_TIMESTAMP=CURRENT_TIMESTAMP;"
-
-    mSQLValueStatement_fp = "(%s, '%s', '%s', %s, %s, '%s', '%s', '%s', '%s', '%s', %s, '%s', %s, '%s', \
-            current_timestamp, current_timestamp),"
-
-    
     def __init__(self, iUid):
         self.mUid = iUid
         WeiboClient.__init__(self, PublicToken.getPublicToken()[0])
@@ -38,47 +32,41 @@ class Followers(WeiboClient):
     #send json data to database
     def _sendToDB(self, iJsonData):
         assert(type(iJsonData) == types.ListType)
+        
         lValueStatement = ""
-        lValueStatement_fp = ""
         for lInterator in iJsonData:
             lValueStatement += (self.mSQLValueStatement % 
                                 (self.mUid,lInterator['id'],0))
-            lValueStatement_fp += (self.mSQLValueStatement_fp % (lInterator['id'], MySQLdb.escape_string(lInterator['screen_name']), MySQLdb.escape_string(lInterator['name']), \
-                        lInterator['province'], lInterator['city'], MySQLdb.escape_string(lInterator["location"]), \
-                        MySQLdb.escape_string(lInterator['description']), MySQLdb.escape_string(lInterator['url']), MySQLdb.escape_string(lInterator["profile_image_url"]), MySQLdb.escape_string(lInterator["domain"]), \
-                        (lInterator["gender"].find('f') == -1), MySQLdb.escape_string(lInterator["avatar_large"]), lInterator["verified"], \
-                        MySQLdb.escape_string(lInterator['verified_reason'])))
-            lSQLStatement = self.mSQLStatement % (lValueStatement[:-1])
-            lSQLStatement_fp = self.mSQLStatement_fp % (lValueStatement_fp[:-1])
-#            try:
-#                FollowersFollowingV(lInterator['id']).process()
-#            except Exception, e:
-#                print ("Error: Cannot crawl data for Weibo User ID=%s because of: %s" % (lInterator['id'], str(e)))
-    
-        #lSQLStatement = MySQLdb.escape_string(lSQLStatement.encode('utf8','ignore'))
-        #print("SQLStatment:%s"%lSQLStatement)
-        #print("lSQLStatement_fp:%s"%lSQLStatement_fp)
+        lSQLStatement = self.mSQLStatement % (lValueStatement[:-1])
         print('SQL for  Followers ready.')
+        
         try:
-            conn = MySQLdb.connect(host=gDBHost, port=gDBPort, user=gDBUser, passwd=gDBPassword, db=gDBSchema, charset="utf8")
+            conn = util.get_crawler_connection()
+            dao = UserDao(conn)
+            dao.insert_users(iJsonData)
+            conn.close()
+        except Exception, e:
+            print 'Error when insert Followers into Database for EUid = %s because of: %s' % (self.mUid, e)
+            
+        try:
+            conn = util.get_crawler_connection()
             cursor = conn.cursor()
             cursor.execute(lSQLStatement)
-            cursor.execute(lSQLStatement_fp)
             cursor.close()
             conn.commit()
             conn.close()
-            print('Store data for Followers to db done.')
+            print('Insert  Followers relationships to db done.')
         except Exception, e:
-            print 'Error when insert Followers into Database for uid = %s because of: %s' % (self.mUid, e) 
+            print 'Error when insert Followers into Database for EUid = %s because of: %s' % (self.mUid, e) 
     
     #fetch from Weibo and call sendToDB
     def process(self):
         iParams = {}
         iParams['uid'] = self.mUid
+        iParams['count'] = 200
         #mPublicToken is a list:['uid', 'access_token']
         iParams['access_token'] = self.mPublicToken[1]
         lJsonResult = self.fetchUsingAPI(self.mAPI, iParams)
-        #print(lJsonResult)
         print('Get Json data for Followers done.')
         if type(lJsonResult) == types.ListType and len(lJsonResult) > 0:
             self._sendToDB(lJsonResult)
