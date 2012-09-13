1a379a1b5a0191a3281217852b020b7fe27a8c88
shezepo0409@gmail.com <shezepo0409@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-13 14:58:27 +0000

    Status improved

--- src/crawler/Status.py
+++ src/crawler/Status.py
@@ -1,16 +1,20 @@
 '''Status's methods
 '''
-import MySQLdb
+
 import json
+import MySQLdb
 import types
-from WeiboClient import WeiboClient
+
+import util
 from conf import *
-from PublicToken import PublicToken
-from util import *
-from dao.repost import RepostDao
-from repost import WeiboRepostAPI
-from dao.comment import CommentDao
 from comment import WeiboCommentAPI
+from dao.comment import CommentDao
+from dao.repost import RepostDao
+from dao.status import StatusDao
+from PublicToken import PublicToken
+from repost import WeiboRepostAPI
+from util import *
+from WeiboClient import WeiboClient
 
 def getNULL(s):
     if s=='':
@@ -19,19 +23,9 @@ def getNULL(s):
         return s
 
 class Status(WeiboClient):
-    mSQLStatement = "INSERT INTO Status \
-                (id_status, create_time, text, \
-                source, is_favorited,is_truncated,\
-                in_reply_to_status_id,in_reply_to_user_id,in_reply_to_screen_name,\
-                mid,reposts_count,comments_count,\
-                id_user,INSERT_TIMESTAMP) \
-                VALUES %s ON DUPLICATE KEY UPDATE \
-                is_favorited=Values(is_favorited), is_truncated=Values(is_truncated), \
-                reposts_count=Values(reposts_count), comments_count=Values(comments_count);\
-                INSERT INTO Status_Counter \
+    mSQLStatement = "INSERT INTO Status_Counter \
                 (id_status,reposts_count,comments_count,INSERT_TIMESTAMP) \
                 values %s;"
-    mSQLValueStatement = "(%s,FROM_UNIXTIME(%s),'%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,current_timestamp),"
     mSQLValue_Status_Counter = "(%s,%s,%s,current_timestamp),"
                 
     mAPI = 'statuses/user_timeline'
@@ -43,25 +37,20 @@ class Status(WeiboClient):
     #send json data to database
     def _sendToDB(self, iJsonData):
         assert(type(iJsonData) == types.ListType)
-        lValueStatement = ""
+        
         lValueStatement2 = ""
         repost_crawler = WeiboRepostAPI(self)
-        repost_result = []
         comment_crawler = WeiboCommentAPI(self)
-        comment_result = []
 
         try:
-            conn = MySQLdb.connect(host=gDBHost, port=gDBPort, user=gDBUser, passwd=gDBPassword, db=gDBSchema, charset="utf8")
+            conn = util.get_crawler_connection()
+            dao = StatusDao
+            dao.insert_statuses(iJsonData)
+            
             for lInterator in iJsonData:
-                lValueStatement += (self.mSQLValueStatement % 
-                                    (lInterator['id'],parse_weibo_time_string(lInterator['created_at']),MySQLdb.escape_string(lInterator['text']),\
-                                    MySQLdb.escape_string(lInterator['source']),lInterator['favorited'],lInterator['truncated'],\
-                                    getNULL(lInterator['in_reply_to_status_id']),getNULL(lInterator['in_reply_to_user_id']),getNULL(lInterator['in_reply_to_screen_name']),\
-                                    lInterator['mid'],lInterator['reposts_count'],lInterator['comments_count'],\
-                                    lInterator['user']['id']))
                 lValueStatement2 += (self.mSQLValue_Status_Counter %
                                      (lInterator['id'],lInterator['reposts_count'],lInterator['comments_count']))
-                lSQLStatement = self.mSQLStatement % (lValueStatement[:len(lValueStatement) - 1],lValueStatement2[:-1])
+                lSQLStatement = self.mSQLStatement % (lValueStatement2[:-1], )
                 
                 repost_result = repost_crawler.get_reposts_of_status(lInterator['id'])
                 comment_result = comment_crawler.get_comments_on_status(lInterator['id'])
@@ -70,9 +59,6 @@ class Status(WeiboClient):
                 comment_dao = CommentDao(conn)
                 comment_dao.insert_comments(comment_result)
                 
-                
-            #lSQLStatement = MySQLdb.escape_string(lSQLStatement.encode('utf8','ignore'))
-            #print(lSQLStatement)
             print('SQL for Status ready.')
             cursor = conn.cursor()
             cursor.execute(lSQLStatement) 
@@ -90,7 +76,6 @@ class Status(WeiboClient):
         #mPublicToken is a list:['uid', 'access_token']
         iParams['access_token'] = self.mPublicToken[1]
         lJsonResult = self.fetchUsingAPI(self.mAPI, iParams)
-        #print(lJsonResult)
         print('Get Json data for status done.')
         if type(lJsonResult) == types.ListType and len(lJsonResult) > 0:
             self._sendToDB(lJsonResult)
--- src/dao/status.py
+++ src/dao/status.py
@@ -4,8 +4,9 @@ Created on Sep 12, 2012
 @author: hidn
 '''
 import MySQLdb
+import types
 
-
+from util import parse_weibo_time_string, parse_long
 
 class StatusDao:
     def __init__(self, conn):
@@ -26,6 +27,44 @@ class StatusDao:
             raise
         
         return rows
+    
+    def insert_statuses(self, j_statuses):
+        assert(type(j_statuses) is types.ListType)
+
+        rows = []
+        for jStatus in j_statuses:
+            rows.append(self._mapRow(jStatus))
+
+        try:
+            conn = self._conn
+            cursor = conn.cursor()
+            cursor.executemany(self.SQL_INSERT_STATUS, rows)
+            cursor.close()
+            conn.commit()
+        except Exception, e:
+            raise
+            #print 'Error when insert WeiboFollower into Database for uid = %s because of: %s' % (self.mUid, e) 
+        
+    @staticmethod
+    def _mapRow(o):
+        return (o['id'], parse_weibo_time_string(o['created_at']), o['text'],
+                o['source'], o['favorited'], o['truncated'],
+                parse_long(o['in_reply_to_status_id']),
+                parse_long(o['in_reply_to_user_id']),
+                parse_long(o['in_reply_to_screen_name']),
+                o['mid'], o['reposts_count'], o['comments_count'], o['user']['id'])
+    
+        
+    SQL_INSERT_STATUS = '''
+INSERT INTO Status
+        (id_status, create_time, text, source, is_favorited, is_truncated,
+        in_reply_to_status_id, in_reply_to_user_id, in_reply_to_screen_name,
+        mid,reposts_count,comments_count, id_user, INSERT_TIMESTAMP)
+VALUES (%s, FROM_UNIXTIME(%s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, current_timestamp)
+ON DUPLICATE KEY UPDATE
+        is_favorited=Values(is_favorited), is_truncated=Values(is_truncated),
+        reposts_count=Values(reposts_count), comments_count=Values(comments_count);
+'''.strip()
     
     SQL_GET_ALL_STATUSES = '''
 SELECT id_status FROM status ORDER BY id_status
