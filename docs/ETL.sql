f8ac0b3641b9a545d2eb6436d4d308ac80233418
hiddenpublic@gmail.com <hiddenpublic@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-13 10:11:59 +0000

    ^ ETL: more

--- docs/ETL.sql
+++ docs/ETL.sql
@@ -1,41 +1,85 @@
 USE biae_raw;
 
-
-CREATE TEMPORARY TABLE biae._fact_enterprise_status_counters
+-- Get count at {status, day} level.
+CREATE TEMPORARY TABLE biae._sc0
 SELECT
-	e.idUser AS enterprise_id
-	DATE(sc.insert_timestamp) AS create_date
-	SUM(sc.reposts_count) AS repost_count
-	SUM(sc.comments_count) AS comment_count
-FROM EUser e
-	JOIN status s ON e.idUser = sc.id_user
-	JOIN status_counter sc ON sc.id_status = s.id_status
-WHERE SC.insert_timestamp IN
+	sc.id_status AS status_id,
+	DATE(sc.insert_timestamp) AS create_date,
+	sc.reposts_count AS repost_count,
+	sc.comments_count AS comment_count
+FROM status_counter sc
+WHERE sc.insert_timestamp IN
 	(
 		SELECT
 			MAX(insert_timestamp)
 		FROM status_counter
 		GROUP BY DATE(insert_timestamp)
-	)
-GROUP BY e.idUser, DATE(sc.insert_timestamp);
+	);
+
+-- Get last day's count (still {status, day} level).
+CREATE TEMPORARY TABLE biae._sc0_ld
+SELECT
+	status_id,
+	ADDDATE(create_date, 1) AS create_date,
+	repost_count AS ld_repost_count,
+	comment_count AS ld_comment_count
+FROM biae._sc0;
 
 
-INSERT INTO biae.fact_enterprise_comment_count
+	
+-- Get count and daily count (still {status, day} level). 
+CREATE TEMPORARY TABLE biae._sc1
+SELECT
+	sc0.status_id,
+	sc0.create_date,
+	sc0.repost_count,
+	sc0.comment_count,
+	sc0.repost_count - sc0_ld.ld_repost_count AS repost_count_today,
+	sc0.comment_count - sc0_ld.ld_comment_count AS comment_count_today
+FROM biae._sc0 AS sc0
+	JOIN biae._sc0_ld AS sc0_ld ON sc0.create_date = sc0_ld.create_date
+;
+
+
+-- Aggregate to {enterprise, day} level.  	
+CREATE TEMPORARY TABLE biae._sc2
+SELECT
+	e.idUser AS enterprise_id,
+  sc.status_id,
+	sc.create_date,
+  repost_count,
+	comment_count,
+	repost_count_today,
+	comment_count_today
+FROM EUser e
+	JOIN status s ON e.idUser = s.id_user
+	JOIN biae._sc1 sc ON sc.status_id = s.id_status
+GROUP BY sc.status_id, sc.create_date;
+
+
+INSERT INTO biae.fact_enterprise_comment_count (enterprise_id, post_id, comment_date, comment_count, comment_count_today)
 SELECT
 	enterprise_id,
-	created_date AS comment_date
-	comment_count
-FROM biae._fact_enterprise_status_counters;
+  status_id AS post_id,
+	create_date AS comment_date,
+	comment_count,
+  comment_count_today
+FROM biae._sc2;
 
 
-INSERT INTO biae.fact_enterprise_repost_count
+INSERT INTO biae.fact_enterprise_repost_count (enterprise_id, post_id, repost_date, repost_count, repost_count_today)
 SELECT
 	enterprise_id,
-	created_date AS repost_date
-	repost_count
-FROM biae._fact_enterprise_status_counters;
+  status_id AS post_id,
+	create_date AS repost_date,
+	repost_count,
+	repost_count_today
+FROM biae._sc2;
 
-DROP TABLE biae._fact_enterprise_status_counters;
+DROP TABLE biae._sc0;
+DROP TABLE biae._sc0_ld;
+DROP TABLE biae._sc1;
+DROP TABLE biae._sc2;
 
 
 
@@ -60,20 +104,21 @@ WHERE uc.insert_timestamp IN
 	
 --- lu_city
 
+
 --- lu_province
 
 	
 --- lu_comment
 INSERT INTO biae.lu_comment
 SELECT
-	sc.comment_id AS comment_id,
-	sc.`text` AS comment_desc,
-	DATE(sc.created_time) AS day_date,
-	sc.commented_status_id AS post_id,
-	sc.user_id AS user_id,
-	s.idUser AS enterprise_id
-FROM status_comment sc
-	JOIN status s ON s.status_id = sc.status_id;
+	c.comment_id AS comment_id,
+	c.`text` AS comment_desc,
+	DATE(c.created_time) AS day_date,
+	c.commented_status_id AS post_id,
+	c.user_id AS user_id,
+	s.id_user AS enterprise_id
+FROM status_comment c
+	JOIN status s ON s.id_status = c.commented_status_id;
 
 --- lu_day: no
 
@@ -88,7 +133,7 @@ FROM EUser e;
 --- ????? lu_event criteria?
 INSERT INTO biae.lu_event
 SELECT
-	? AS event_id,
+	s.id_user AS event_id,
 	s.`text` AS event_desc,
 	DATE(s.create_time) AS day_date,
 	s.id_status AS post_id,
@@ -105,11 +150,39 @@ WHERE ?;
 --- lu_month: no
 
 --- lu_post
-
+INSERT INTO biae.lu_post
+SELECT
+	`id_status` AS post_id,
+	`text` AS post_desc,
+	DATE(create_time) AS day_date,
+	id_user AS enterprise_id
+FROM status;
 
 --- lu_repost
+INSERT INTO biae.lu_repost
+SELECT
+	r.repost_id,
+	r.`text` AS repost_desc,
+	DATE(r.created_time) AS day_date,
+	r.retweeted_status_id AS post_id,
+	r.user_id,
+	s.id_user AS enterprise_id
+FROM repost r JOIN status s ON s.id_status = r.retweeted_status_id;
+
 
 --- lu_user
+INSERT INTO biae.lu_user
+SELECT
+	fo.id_follower AS user_id,
+	u.screen_name AS user_name,
+	u.gender AS gender_id,
+	10000 * u.province + u.city AS city_id,
+	u.followers_count AS fans_count,
+	u.verified AS has_v
+FROM followers fo JOIN euser e ON e.idUser = fo.id_user
+	JOIN WeiboUser u ON u.idUser = fo.id_follower;
+
+
 
 
 --- lu_week: no
@@ -122,6 +195,20 @@ WHERE ?;
 
 
 
+
+--- lu_qualified_user
+--- SELECT 
+
+
+
+--- rel_qualified_fan
+SELECT
+FROM 
+
+
 --- rel_user_interest
+
+
+
 
 
