6b9f5a6d7224cc85969463a30815ce0674a6e26a
michael8090@gmail.com <michael8090@gmail.com@8b594c1c-8417-452e-974a-0b9ce21153d1>
2012-09-19 08:30:59 +0000

    remove the previous multi-thread completion specialized for comment and repost.

--- src/crawler/main.py
+++ src/crawler/main.py
@@ -1,28 +1,95 @@
 from Multi_Thread_Repost_Comment import *
 import util
 
+import util
+from comment import WeiboCommentAPI
+from dao.user import UserDao
+from dao.usercounter import UserCounterDao
+from dao.follower import FollowerDao
+from dao.status import StatusDao
+from dao.statuscounter import StatusCounterDao
+from dao.repost import RepostDao
+from dao.comment import CommentDao
+from EUser import EUser
+from Followers import Followers
+from repost import WeiboRepostAPI
+from Status import Status
+from Tag import Tag
+from WeiboUser import WeiboUser
+from VFriends import VFriends
+
 #from dao.repost import RepostDao
 #from dao.comment import CommentDao
 #from PublicToken import PublicToken
 #from WeiboClient import WeiboClient
-import threading
+#import threading
+
+#if __name__ == '__main__':
+#    params = getParams()
+#    threadCount = len(params['repostCrawlers'])
+#    threads = []
+#    for i in range(0,threadCount):
+#        athread = threading.Thread(target = crawl_repost_and_comment, args = (i,params))
+#        threads.append(athread)
+#    for athread in threads:
+#        athread.start()
+#    
+#    isRunning = True
+#    while isRunning: 
+#        isRunning = False
+#        for athread in threads:
+#            if athread.isAlive():
+#                isRunning = True
 
 if __name__ == '__main__':
-    params = getParams()
-    threadCount = len(params['repostCrawlers'])
-    threads = []
-    for i in range(0,threadCount):
-        athread = threading.Thread(target = crawl_repost_and_comment, args = (i,params))
-        threads.append(athread)
-    for athread in threads:
-        athread.start()
     
-    isRunning = True
-    while isRunning: 
-        isRunning = False
-        for athread in threads:
-            if athread.isAlive():
-                isRunning = True
+    weiboClient = util.get_weibo_client()
+    repostCrawler = WeiboRepostAPI(weiboClient)
+    commentCrawler = WeiboCommentAPI(weiboClient)
+#    userCrawler = WeiboUser(weiboClient)
+#    followerCrawler = Followers(weiboClient)
+#    statusCrawler = Status(weiboClient)
+    
+    try:
+        conn = util.get_crawler_connection()
+    except Exception, e:
+        print ("ERROR: Init conn fail: %s" % (str(e), ))
+        
+#    userDao = UserDao(conn)
+#    userCounterDao = UserCounterDao(conn)
+#    followerDao = FollowerDao(conn)
+#    statusDao = StatusDao(conn)
+#    statusCounterDao = StatusCounterDao(conn)
+    repostDao = RepostDao(conn)
+    commentDao = CommentDao(conn)    
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute('select id_status from Status') 
+        statuses = cursor.fetchall()
+        cursor.close()
+    except Exception, e:
+        print ('Error when load Status from DB')
+    print('Load Status from DB done.')
+    
+
+    
+    for status in statuses:
+        try:
+            reposts = repostCrawler.get_reposts_of_status(status[0])
+            repostDao.insert_reposts(reposts)
+            print '.',
+        except Exception, e:
+            print ("ERROR: Insert reposts fail: %s" % (str(e), ))
+            util.print_full_exception()
+        try:
+            comments = commentCrawler.get_comments_on_status(status[0])
+            commentDao.insert_comments(comments)
+            print '.',
+        except Exception, e:
+            print ("ERROR: Insert comments fail: %s" % (str(e), ))
+            util.print_full_exception()
+    print('All done.')
         
     
     
